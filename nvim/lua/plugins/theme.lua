f2d8b93210353f67dd3a6fb13ac78290a2f365c2
Athul Anoop <athulanoop2020@gmail.com>
2024-04-28 22:31:15 +0530

    Added transparency to neovim window

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,10 +1,13 @@
 return {
 	"catppuccin/nvim",
-	name = "theme",
+	name = "catppuccin",
 	lazy = false,
 	priority = 999,
 	config = function()
+		require("catppuccin").setup({
+			transparent_background = true,
+		})
+
 		vim.cmd("colorscheme catppuccin")
-		require("catppuccin").setup({})
 	end,
 }
