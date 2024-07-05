ce5937c9ff57496273d549b32844a1fd0ff498ae
Athul Anoop <athulanoop2020@gmail.com>
2024-07-05 21:31:16 +0530

    Update nvim-cmp.lua

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,47 +1,49 @@
-local config =  function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
+local config = function()
+	local cmp = require("cmp")
+	local luasnip = require("luasnip")
+	local lspkind = require("lspkind")
 
-		require("luasnip/loaders/from_vscode").lazy_load()
+	require("luasnip/loaders/from_vscode").lazy_load()
 
-		vim.opt.completeopt = "menu,menuone,preview,noselect"
+	vim.opt.completeopt = "menu,menuone,preview,noselect"
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				luasnip.lsp_expand(args.body)
+			end,
+		},
+		mapping = cmp.mapping.preset.insert({
+			["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+			["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+			["<C-e>"] = cmp.mapping.abort(), -- close completion window
+			["<CR>"] = cmp.mapping.confirm({ select = false }),
+		}),
+		-- sources for autocompletion in order of preference
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp" }, -- lsp
+			{ name = "luasnip" }, -- snippets
+            { name = "supermaven"}, -- supermaven code assistant
+			{ name = "buffer" }, -- text within current buffer
+			{ name = "path" }, -- file system paths
+
+		}),
+		-- configure lspkind for vs-code like icons
+		formatting = {
+			format = lspkind.cmp_format({
+				maxwidth = 50,
+				ellipsis_char = "...",
 			}),
-			-- sources for autocompletion in order of preference
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" }, -- lsp
-				{ name = "luasnip" }, -- snippets
-				{ name = "buffer" }, -- text within current buffer
-				{ name = "path" }, -- file system paths
-			}),
-			-- configure lspkind for vs-code like icons
-			formatting = {
-				format = lspkind.cmp_format({
-					maxwidth = 50,
-					ellipsis_char = "...",
-				}),
-			},
-		})
-	end
+		},
+	})
+end
 
 return {
 	"hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+	event = "InsertEnter",
 	config = config,
 	dependencies = {
 		{
@@ -53,9 +55,9 @@ return {
 			build = "make install_jsregexp",
 		},
 		"onsails/lspkind.nvim",
-        "hrsh7th/cmp-buffer", -- source for text in buffer
-        "hrsh7th/cmp-path", -- source for file system paths
-        "saadparwaiz1/cmp_luasnip", -- autocompletion
-        "rafamadriz/friendly-snippets", -- snippets
+		"hrsh7th/cmp-buffer", -- source for text in buffer
+		"hrsh7th/cmp-path", -- source for file system paths
+		"saadparwaiz1/cmp_luasnip", -- autocompletion
+		"rafamadriz/friendly-snippets", -- snippets
 	},
 }
