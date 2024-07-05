fb0b5a1431bfd2630d56a93193a791def40f374e
Athul Anoop <athulanoop2020@gmail.com>
2024-07-05 21:29:52 +0530

    disabled rest.nvim since it is no longer functional

--- nvim/lua/plugins/rest.lua
+++ nvim/lua/plugins/rest.lua
@@ -1,9 +1,20 @@
 return {
-	"rest-nvim/rest.nvim",
-	ft = "http",
-	lazy = false,
-	dependencies = { "luarocks.nvim" },
-	config = function()
-		require("rest-nvim").setup()
-	end,
+	-- "rest-nvim/rest.nvim",
+	-- ft = "http",
+	-- dependencies = { "luarocks.nvim" },
+	-- config = function()
+	-- 	vim.keymap.set(
+	-- 		"n",
+	-- 		"<leader>ar",
+	-- 		"<cmd>Rest run<cr>",
+	-- 		{ desc = "Run request under the cursor", silent = true, noremap = true }
+	-- 	)
+	-- 	vim.keymap.set(
+	-- 		"n",
+	-- 		"<leader>al",
+	-- 		"<cmd>Rest run last<cr>",
+	-- 		{ desc = "Re-run latest request", silent = true, noremap = true }
+	-- 	)
+	-- 	require("rest-nvim").setup()
+	-- end,
 }
