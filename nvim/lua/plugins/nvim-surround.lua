183dd6f68b52ea4041a2ae73b6474dee16426d0c
Athul Anoop <athulanoop2020@gmail.com>
2024-07-23 23:42:07 +0530

    udpated lazy plugins

--- nvim/lua/plugins/nvim-surround.lua
+++ nvim/lua/plugins/nvim-surround.lua
@@ -1,7 +1,6 @@
 return {
 	"kylechui/nvim-surround",
-	version = "*", -- Use for stability; omit to use `main` branch for the latest features
-	event = "VeryLazy",
+	event = "InsertEnter",
 	config = function()
 		require("nvim-surround").setup({
 			-- Configuration here, or leave empty to use defaults
