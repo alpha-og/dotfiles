0164a180aa48fce8bdeccc546c324637285966eb
Athul Anoop <athulanoop2020@gmail.com>
2024-12-02 17:38:17 +0530

    update: pinned supermaven to commit `d7125` due to stability issues in newer versions and updated keymaps

--- nvim/lua/plugins/supermaven-nvim.lua
+++ nvim/lua/plugins/supermaven-nvim.lua
@@ -1,13 +1,19 @@
 return {
 	"supermaven-inc/supermaven-nvim",
+	commit = "d7125",
 	lazy = false,
 	config = function()
 		require("supermaven-nvim").setup({
 			keymaps = {
-				accept_suggestion = "<Tab>",
+				accept_suggestion = "<C-l>",
 				clear_suggestion = "<C-]>",
-				accept_word = "<C-j>",
+				accept_word = "<C-m>",
 			},
+			-- disable_inline_completion = true,
+			-- 	ignore_filetypes = {
+			-- 		rust = true,
+			-- 		-- c = true,
+			-- 	},
 		})
 	end,
 }
