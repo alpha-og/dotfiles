c0f3a81a20b8312f9ceeb7ea518e4ab501d324af
Athul Anoop <athulanoop2020@gmail.com>
2024-12-18 18:50:44 +0530

    add: plugin to display notifications more pleasantly

---
+++ nvim/lua/config/plugins/appearance/notify.lua
@@ -0,0 +1,23 @@
+return {
+	"rcarriga/nvim-notify",
+	config = function()
+		require("notify").setup({
+			background_colour = "#000000",
+			timeout = 500,
+			render = "compact",
+		})
+
+		vim.keymap.set(
+			"n",
+			"<leader>vn",
+			"<CMD>Telescope notify<CR>",
+			{ noremap = true, silent = true, desc = "View Notification History" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>cn",
+			require("notify").dismiss,
+			{ noremap = true, silent = true, desc = "Dismiss Notification" }
+		)
+	end,
+}
