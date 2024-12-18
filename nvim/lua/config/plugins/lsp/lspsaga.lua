da8bd0cdb1e0bc9fbcd180617eebd3ab6149a158
Athul Anoop <athulanoop2020@gmail.com>
2024-12-18 18:53:04 +0530

    add: plugin for better lsp features (currently only using code actions)

---
+++ nvim/lua/config/plugins/lsp/lspsaga.lua
@@ -0,0 +1,9 @@
+return {
+    "nvimdev/lspsaga.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
+    event = "LspAttach",
+    config = true,
+}
