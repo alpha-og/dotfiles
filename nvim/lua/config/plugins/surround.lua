ca4ca4275e6f5591a0b1e5e0375d864d6f3b7e39
Athul Anoop <athulanoop2020@gmail.com>
2024-12-18 18:49:28 +0530

    update: moved to plugins/utils

--- nvim/lua/config/plugins/surround.lua
+++
@@ -1,6 +0,0 @@
--- surround selections with desired characters
-return {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-}
