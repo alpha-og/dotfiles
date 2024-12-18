a74b16f16fb919313ad746ee7b96e82be3b11bc5
Athul Anoop <athulanoop2020@gmail.com>
2024-12-18 18:52:08 +0530

    update: keymaps

--- nvim/lua/config/plugins/lsp/cmp.lua
+++ nvim/lua/config/plugins/lsp/cmp.lua
@@ -8,30 +8,32 @@ local config = function()
 	vim.opt.completeopt = "menu,menuone,preview,noselect"
 
 	cmp.setup({
+		mapping = cmp.mapping.preset.insert({
+			["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }), -- previous suggestion
+			["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }), -- next suggestion
+			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+			["<C-c>"] = cmp.mapping.abort(), -- close completion window
+			["<CR>"] = cmp.mapping(
+				cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+				{ "i", "c" }
+			), -- confirm completion
+		}),
+		-- sources for autocompletion in order of preference
+		sources = {
+			{ name = "nvim_lsp" }, -- lsp
+			{ name = "luasnip" }, -- snippets
+			{ name = "path" }, -- file system paths
+			{ name = "crates" }, -- crates.io
+			{ name = "buffer" }, -- text within current buffer
+		},
+		-- configure lspkind for vs-code like icons
 		snippet = {
 			expand = function(args)
 				luasnip.lsp_expand(args.body)
 			end,
 		},
-		mapping = cmp.mapping.preset.insert({
-			["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-			["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-			["<C-e>"] = cmp.mapping.abort(), -- close completion window
-			["<CR>"] = cmp.mapping.confirm({ select = false }),
-		}),
-		-- sources for autocompletion in order of preference
-		sources = cmp.config.sources({
-			{ name = "nvim_lsp" }, -- lsp
-			{ name = "luasnip" }, -- snippets
-			{ name = "path" }, -- file system paths
-			{ name = "supermaven" }, -- supermaven code assistant
-			{ name = "crates" }, -- crates.io
-			{ name = "buffer" }, -- text within current buffer
-		}),
-		-- configure lspkind for vs-code like icons
 		formatting = {
 			format = lspkind.cmp_format({
 				mode = "symbol",
@@ -58,7 +60,7 @@ return {
 			-- install jsregexp (optional!).
 			build = "make install_jsregexp",
 		},
-		"onsails/lspkind.nvim",
+		"onsails/lspkind.nvim", -- lsp kind icons
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
 		"saadparwaiz1/cmp_luasnip", -- autocompletion
