1f40f0d2fee2a927323b7fe1b1280a3cbe292be5
Athul Anoop <athulanoop2020@gmail.com>
2024-12-19 09:16:31 +0530

    add: commenstring computation plugin

---
+++ nvim/lua/config/plugins/utils/comments.lua
@@ -0,0 +1,16 @@
+-- plugin for computing commentstring
+
+return {
+	"JoosepAlviste/nvim-ts-context-commentstring",
+	config = function()
+		require("ts_context_commentstring").setup({
+			enable_autocmd = false,
+		})
+		-- integration for neovim's native commenting
+		local get_option = vim.filetype.get_option
+		vim.filetype.get_option = function(filetype, option)
+			return option == "commentstring" and require("ts_context_commentstring.internal").calculate_commentstring()
+				or get_option(filetype, option)
+		end
+	end,
+}
