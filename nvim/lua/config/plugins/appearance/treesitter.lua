03f41d5bd531bafdd1a2f74233de1e60d09239e9
Athul Anoop <athulanoop2020@gmail.com>
2024-12-30 20:05:02 +0530

    update: commented out dysfunctional ejs template highlighting

--- nvim/lua/config/plugins/appearance/treesitter.lua
+++ nvim/lua/config/plugins/appearance/treesitter.lua
@@ -29,6 +29,16 @@ local ensure_installed = {
 	"vimdoc",
 }
 local config = function()
+	-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+	-- parser_config.embedded_template = {
+	-- 	install_info = {
+	-- 		url = "https://github.com/tree-sitter/tree-sitter-embedded-template",
+	-- 		files = { "src/parser.c" },
+	-- 		requires_generate_from_grammar = true,
+	-- 	},
+	-- 	filetype = "ejs",
+	-- }
+
 	require("nvim-treesitter.configs").setup({
 		ensure_installed = ensure_installed,
 		auto_install = true,
