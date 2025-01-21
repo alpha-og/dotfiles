793690b47e1232fca2a72118908905df53e58966
Athul Anoop <athulanoop2020@gmail.com>
2025-01-21 23:28:09 +0530

    add: plugin for smart movement between neovim and wezterm

---
+++ nvim/lua/config/plugins/utils/smart-splits.lua
@@ -0,0 +1,20 @@
+return {
+	"mrjones2014/smart-splits.nvim",
+	config = function()
+		vim.keymap.set("n", "<S-C-h>", require("smart-splits").resize_left)
+		vim.keymap.set("n", "<S-C-j>", require("smart-splits").resize_down)
+		vim.keymap.set("n", "<S-C-k>", require("smart-splits").resize_up)
+		vim.keymap.set("n", "<S-C-l>", require("smart-splits").resize_right)
+		-- moving between splits
+		-- vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+		-- vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+		-- vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+		-- vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+		-- vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
+		-- swapping buffers between windows
+		vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+		vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+		vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+		vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
+	end,
+}
