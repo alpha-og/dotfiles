18a68533bee5de3b1f7f207b9e6e0d9606844e16
Athul Anoop <athulanoop2020@gmail.com>
2025-01-24 22:21:56 +0530

    update: moved event based behaviour to root config file

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,14 @@
 local wezterm = require("wezterm")
 local background = require("background")
 local keymaps = require("keymaps")
-require("plugins.plugins")
 
 local config = wezterm.config_builder()
+local function basename(s)
+	return string.gsub(s, "(.*[/\\])(.*)", "%2")
+end
 
 config.leader = keymaps.leader
-config.keys = keymaps.keys
+config.keys = {}
 
 -- font
 config.font = wezterm.font("Hack Nerd Font")
@@ -28,6 +30,62 @@ config.use_fancy_tab_bar = false
 -- performance
 -- config.max_fps = 120 -- set max fps to 120
 -- config.prefer_egl = true -- prefer egl
+
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
+---@param opts? { interval_seconds: integer?, save_workspaces: boolean?, save_windows: boolean?, save_tabs: boolean? }
+resurrect.periodic_save({ interval_seconds = 60, save_workspaces = true, save_windows = true, save_tabs = true })
+
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+workspace_switcher.workspace_formatter = function(label)
+	return wezterm.format({
+		{ Attribute = { Italic = true } },
+		-- { Foreground = { Color = colors.colors.ansi[3] } },
+		-- { Background = { Color = colors.colors.background } },
+		{ Text = "󱂬 : " .. label },
+	})
+end
+
+wezterm.on("smart_workspace_switcher.workspace_switcher.created", function(window, path, label)
+	window:gui_window():set_right_status(wezterm.format({
+		{ Attribute = { Intensity = "Bold" } },
+		-- { Foreground = { Color = colors.colors.ansi[5] } },
+		{ Text = basename(path) .. "  " },
+	}))
+	local workspace_state = resurrect.workspace_state
+
+	workspace_state.restore_workspace(resurrect.load_state(label, "workspace"), {
+		window = window,
+		relative = true,
+		restore_text = true,
+		on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+	})
+end)
+
+wezterm.on("smart_workspace_switcher.workspace_switcher.chosen", function(window, path, label)
+	wezterm.log_info(window)
+	window:gui_window():set_right_status(wezterm.format({
+		{ Attribute = { Intensity = "Bold" } },
+		-- { Foreground = { Color = colors.colors.ansi[5] } },
+		{ Text = basename(path) .. "  " },
+	}))
+end)
+
+wezterm.on("smart_workspace_switcher.workspace_switcher.selected", function(window, path, label)
+	wezterm.log_info(window)
+	local workspace_state = resurrect.workspace_state
+	resurrect.save_state(workspace_state.get_workspace_state())
+	resurrect.write_current_state(label, "workspace")
+end)
+
+wezterm.on("smart_workspace_switcher.workspace_switcher.start", function(window, _)
+	wezterm.log_info(window)
+end)
+wezterm.on("smart_workspace_switcher.workspace_switcher.canceled", function(window, _)
+	wezterm.log_info(window)
+end)
+wezterm.on("gui-startup", resurrect.resurrect_on_gui_startup)
+
+keymaps.setup(wezterm, config)
 
 -- return the configuration
 return config
