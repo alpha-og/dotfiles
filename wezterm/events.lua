6818ea009c4f6fae1a7b75fb892700a2f857a3c4
Athul Anoop <athulanoop2020@gmail.com>
2025-01-24 19:40:51 +0530

    update: convenience events

--- wezterm/events.lua
+++ wezterm/events.lua
@@ -27,4 +27,24 @@ wezterm.on("augment-command-palette", function(window, pane)
 	}
 end)
 
+-- loads the state whenever I create a new workspace
+wezterm.on("smart_workspace_switcher.workspace_switcher.created", function(window, path, label)
+	local workspace_state = resurrect.workspace_state
+
+	workspace_state.restore_workspace(resurrect.load_state(label, "workspace"), {
+		window = window,
+		relative = true,
+		restore_text = true,
+		on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+	})
+end)
+
+-- Saves the state whenever I select a workspace
+wezterm.on("smart_workspace_switcher.workspace_switcher.selected", function(window, path, label)
+	local workspace_state = resurrect.workspace_state
+	resurrect.save_state(workspace_state.get_workspace_state())
+end)
+
+wezterm.on("gui-startup", resurrect.resurrect_on_gui_startup)
+
 return M
