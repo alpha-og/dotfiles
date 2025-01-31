a0f4d4f6510e00b1dadcc2f99e96634bd6c65d37
Athul Anoop <athulanoop2020@gmail.com>
2025-01-31 17:27:19 +0530

    update: add completion sources for obsidian with blink nvim using nvim-cmp compatibility

--- nvim/lua/config/plugins/lsp/blink.lua
+++ nvim/lua/config/plugins/lsp/blink.lua
@@ -11,6 +11,16 @@ return {
 			build = "make install_jsregexp",
 		},
 		"onsails/lspkind-nvim",
+
+		{
+			"saghen/blink.compat",
+			-- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+			version = "*",
+			-- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+			lazy = true,
+			-- make sure to set opts so that lazy.nvim calls blink.compat's setup
+			opts = {},
+		},
 	},
 
 	-- use a release tag to download pre-built binaries
@@ -44,5 +54,18 @@ return {
 				-- border = "rounded",
 			},
 		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer", "obsidian", "obsidian_new", "obsidian_tags" },
+
+			providers = {
+				obsidian = { name = "obsidian", module = "blink.compat.source" },
+				obsidian_new = { name = "obsidian_new", module = "blink.compat.source" },
+				obsidian_tags = { name = "obsidian_tags", module = "blink.compat.source" },
+			},
+
+			cmdline = {},
+		},
 	},
+
+	opts_extend = { "sources.default" },
 }
