8b144d851737a44652ecf8929d566a538eb5e139
Athul Anoop <athulanoop2020@gmail.com>
2025-05-09 19:09:21 +0530

    fix: keymap

--- nvim/lua/config/plugins/utils/terminotaur.lua
+++ nvim/lua/config/plugins/utils/terminotaur.lua
@@ -7,6 +7,6 @@ return {
 		vim.keymap.set("n", "<leader>tv", "<cmd>OpenNewVerticalTerminal<cr>", { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>th", "<cmd>OpenNewHorizontalTerminal<cr>", { noremap = true, silent = true })
 		vim.keymap.set("t", "<esc><esc>", "<C-\\><C-n>", { noremap = true, silent = true })
-		vim.keymap.set("t", "<C-[><C-]>", "<C-\\><C-n>", { noremap = true, silent = true })
+		vim.keymap.set("t", "<C-[><C-[>", "<C-\\><C-n>", { noremap = true, silent = true })
 	end,
 }
