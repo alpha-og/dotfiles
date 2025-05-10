14d5ed778fffaa7dc3cf31f186602c1c21c30aaa
Athul Anoop <athulanoop2020@gmail.com>
2025-05-10 08:48:48 +0530

    fix: match updated config spec

--- nvim/lua/config/plugins/lsp/blink.lua
+++ nvim/lua/config/plugins/lsp/blink.lua
@@ -62,8 +62,6 @@ return {
 				obsidian_new = { name = "obsidian_new", module = "blink.compat.source" },
 				obsidian_tags = { name = "obsidian_tags", module = "blink.compat.source" },
 			},
-
-			cmdline = {},
 		},
 	},
 
