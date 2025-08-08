c1d1eb9fc27aefe093f2437ddfe86d661a4bd93e
Athul Anoop <athulanoop2020@gmail.com>
2025-08-08 06:27:02 +0530

    add: plugin to open lazydocker from within neovim

---
+++ nvim/lua/config/plugins/utils/lazydocker.lua
@@ -0,0 +1,20 @@
+-- plugin for lazydocker support within neovim
+return {
+	"mgierada/lazydocker.nvim",
+	dependencies = { "akinsho/toggleterm.nvim" },
+	config = function()
+		require("lazydocker").setup({
+			border = "curved", -- valid options are "single" | "double" | "shadow" | "curved"
+		})
+	end,
+	event = "BufRead",
+	keys = {
+		{
+			"<leader>ld",
+			function()
+				require("lazydocker").open()
+			end,
+			desc = "Open Lazydocker floating window",
+		},
+	},
+}
