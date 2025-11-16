392c897c44e73fce48bb43d436097094d804c2a6
Athul Anoop <athulanoop2020@gmail.com>
2025-11-16 15:41:55 +0530

    feat: change tabstop and tabwidths in favor of compactness

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,9 +1,9 @@
 local opt = vim.opt -- reference variable to the global vim.opt object
 
 -- Tab / Indentation
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.softtabstop = 8
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 4
 opt.expandtab = true
 opt.smartindent = true
 
