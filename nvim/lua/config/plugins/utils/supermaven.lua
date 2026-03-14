dd7917c256277e218e0f8b72080c464591fb4928
Athul Anoop <athulanoop2020@gmail.com>
2026-03-14 12:08:49 +0530

    chore: formatting

--- nvim/lua/config/plugins/utils/supermaven.lua
+++ nvim/lua/config/plugins/utils/supermaven.lua
@@ -1,31 +1,32 @@
 -- super fast copilot
 
 return {
-    "supermaven-inc/supermaven-nvim",
-    commit = "d7125",
-    config = function()
-        require("supermaven-nvim").setup({
-            keymaps = {
-                accept_suggestion = "<C-l>",
-                clear_suggestion = "<C-]>",
-                accept_word = "<C-y>",
-            },
-            disable_keymaps = true,
-            ignore_filetypes = {
-                c = true,
-                cpp = true,
-                rust = true,
-            },
-        })
-        vim.keymap.set("i", "<C-l>", function()
-            local luasnip = require("luasnip")
-            local suggestion = require("supermaven-nvim.completion_preview")
+	"supermaven-inc/supermaven-nvim",
+	-- commit = "d7125",
+	config = function()
+		require("supermaven-nvim").setup({
+			log_level = "debug",
+			keymaps = {
+				accept_suggestion = "<C-l>",
+				clear_suggestion = "<C-]>",
+				accept_word = "<C-y>",
+			},
+			disable_keymaps = true,
+			ignore_filetypes = {
+				c = true,
+				cpp = true,
+				rust = true,
+			},
+		})
+		vim.keymap.set("i", "<C-l>", function()
+			local luasnip = require("luasnip")
+			local suggestion = require("supermaven-nvim.completion_preview")
 
-            if luasnip.expandable() then
-                luasnip.expand()
-            elseif suggestion.has_suggestion() then
-                suggestion.on_accept_suggestion()
-            end
-        end, {})
-    end,
+			if luasnip.expandable() then
+				luasnip.expand()
+			elseif suggestion.has_suggestion() then
+				suggestion.on_accept_suggestion()
+			end
+		end, {})
+	end,
 }
